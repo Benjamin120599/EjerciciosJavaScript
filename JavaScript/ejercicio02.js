dabc48d6e5d97897b5986ead3a044ae82d094c86
Benjamin Viramontes <benja120599@gmail.com>
2020-10-02 21:00:15 -0500

    El ejercicio02 está completo

--- JavaScript/ejercicio02.js
+++ JavaScript/ejercicio02.js
@@ -1,11 +1,22 @@
 function octal() {
-
+    let decimal = document.getElementById('caja_decimal').value;
+    var a = new Number(decimal); 
+    return a.toString(8);
 }
 
 function hexadecimal() {
-
+    let decimal = document.getElementById('caja_decimal').value;
+    var a = new Number(decimal); 
+    return a.toString(16);
 }
 
 function binario() {
-    
+    let decimal = document.getElementById('caja_decimal').value;
+    var a = new Number(decimal); 
+    return a.toString(2);
+}
+
+function prueba() {
+    var x = 7;
+    return x.toString(2)
 }
